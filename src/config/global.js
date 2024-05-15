beabefa90f7639f9f238a4397cb1eaa5d2235b4c
Edward Leonardo Pico Cabra <63673074+Leonardo-Pico@users.noreply.github.com>
2024-05-14 20:52:08 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -118,7 +118,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/228118_CF03_DU.pdf',
       },
       {
         icono: 'fas fa-download',
